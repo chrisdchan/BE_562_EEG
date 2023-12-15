877803fea94631312a5db787a80da04596c5c8e8
Chris <christopherdchan02@gmail.com>
2023-12-15 14:49:49 -0500

    Add testing

--- Kruskal.py
+++ Kruskal.py
@@ -10,7 +10,7 @@ def find_max_spanning_tree(G):
         np.Array - Adjacency Matrix of MST with all edges equal to 1
     '''
     N, _ = np.shape(G)
-    spanning_tree = np.zeros_like(G)
+    spanning_tree = np.zeros_like(G, dtype=bool)
 
     union_find = UnionFind(N)
     edges = [(G[node1, node2], node1, node2) for node1 in range(N) for node2 in range(node1)]
@@ -31,15 +31,52 @@ def find_max_spanning_tree(G):
 
     return spanning_tree
 
-if __name__ == '__main__':
-    A = np.array([
-        [0, 3, 5],
-        [3, 0, 7],
-        [5, 7, 0],
-    ], dtype=float)
+def compute_mst(mi_path):
+    '''
+    Args:
+        mi_path: path to a mutual info graph .npy file
+        mst_path: path to save mst to as a .csv
+    '''
+    mutual_info_graph = np.load(mi_path)
+    mst = find_max_spanning_tree(mutual_info_graph)
+    return mst
 
-    mst = find_max_spanning_tree(A)
-    print(mst)
+def get_network_parents(mst, start_node=0):
+
+    stack = []
+
+    seen = {} 
+    stack.append(start_node)
+    n, _ = mst.shape
+    parents = np.ones(n) * -1
+
+    while len(stack) > 0:
+        node = stack.pop()
+        seen[node] = True
+        children = np.nonzero(mst[node])[0]
+        for child in children:
+            if child not in seen:
+                assert parents[child] == -1
+                parents[child] = node
+                stack.append(child)
+    
+    return parents
+
+def save_graph_npy(graph, path):
+    np.save(path, graph)
+    print(f'Graph computed and saved to {path}')
+
+def save_graph_csv(graph, path):
+    np.savetxt(path, graph, delimiter=',')
+    print(f'Graph computed and saved to {path}')
+
+
+if __name__ == '__main__':
+    mi_path = "data/I.npy"
+    network_path = "data/parents.npy"
+    mst = compute_mst(mi_path)
+    parents = get_network_parents(mst)
+    save_graph_npy(parents, network_path)
 
     
 
