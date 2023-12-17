70c5648509d7cd816a61b9eba4d503338e74aaef
Chris <christopherdchan02@gmail.com>
2023-12-17 18:41:58 -0500

    Revert correct version of naieve bayes

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,32 +1,24 @@
 import numpy as np
-from scipy.stats import norm
+from scipy.stats import norm, multivariate_normal
 
 class NaiveBayes():
-
-    def __init__(self, single_normal, num_classes=2, num_dims=300):
+    def __init__(self, single_normal, priors, num_classes=2, num_dims=300):
         self.single_normal = single_normal
         self.num_classes = num_classes
         self.num_dims = num_dims
+        self.priors = priors
 
     def classify(self, x):
-        x_given_class = np.zeros((x))
+        likelihood = np.zeros((self.num_classes, ))
 
         for c in range(self.num_classes):
             mus = self.single_normal[:, c, 0]
             sigmas = self.single_normal[:, c, 1]
             node_probs = norm.pdf(x, mus, sigmas)
-            x_given_class[c] = np.prod(node_probs)
+            log_node_probs = np.log(node_probs)
+            likelihood[c] = np.sum(log_node_probs) + self.priors[c]
 
-        return np.argmax(x_given_class)
-
-    def classi(self, x):
-        node_probs = norm.pdf(
-            x, 
-            self.single_normal[:, :, 0], 
-            self.single_normal[:, :, 1]
-        )
-        x_given_class = np.prod(node_probs, axis=0)
-        return np.argmax(x_given_class)
+        return np.argmax(likelihood)
 
     def test(self, X, y):
         N, _ = X.shape
@@ -34,15 +26,10 @@ class NaiveBayes():
 
         for i in range(N):
             x = X[i]
-            prediction = self.classi(x)
+            prediction = self.classify(x)
+
             if prediction == y[i]:
                 correct += 1
 
-            if i % 100 == 0 and i != 0:
+            if i % 500 == 0 and i != 0:
                 print(f'Running Accuracy is {correct / i} for {i} samples')
-
-
-
-
-        
-        
